35ff53d6eb9d0300e156b4a074d1d0c9c6864b2b
Patrick Stone <89548743+Pstone2@users.noreply.github.com>
2024-07-02 16:12:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-
+from setuptools import setup
+setup(name='hello',
+      version='0.1',
+      description='print hello a number of times',
+      url='http://github.com/package_hello/hello',
+      author='Patrick',
+      author_email='none',
+      license='MIT',
+      packages=['hello'])
